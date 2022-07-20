c7e39db6917aa6ec6962c7c18201a94609626e47
Gustavo Romero <gromero@moveapps.cl>
2022-07-19 21:54:07 -0400

    fundamentos - nestJS controllers (Query Params Decorator - Query String) pt2

--- controllers-II/src/product/product.controller.ts
+++ controllers-II/src/product/product.controller.ts
@@ -1,10 +1,34 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put, Query } from '@nestjs/common';
 
 @Controller('product')
 export class ProductController {
+  /* 
+    http://localhost:3000/product?page=1&perPAge=10&search=iphone
+  */
   @Get()
-  getAll() {
-    return 'Array of products';
+  getAll(
+    //A través del decorador Query podemos obtener los params que vienen a traves de la url
+    //Hay dos formas de traerlos, la primera en un solo objeto y la segunda propiedad por propiedad  
+    @Query() query: any,
+    @Query('page') _page: string,
+    @Query('perPage') _perPage: string,
+    @Query('search') search: string
+  ) {
+
+    console.log(query);
+
+    //const { page, perPAge, search } = query;
+
+    //Parsear los valores de string a número
+    const page = Number(_page);
+    const perPage = Number(_perPage);
+
+    const metadata = { page, perPage, search }
+    return {
+      message: 'Array of products',
+      data: [],
+      metadata,
+    };
   }
 
   @Get()
